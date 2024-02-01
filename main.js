7cb1a3ad8610684754ae2023587f1a2117c69a1d
Mariusm.koder <mariusm.koder@gmail.com>
2024-02-01 13:06:48 +0100

    task started

--- main.js
+++ main.js
@@ -7,3 +7,15 @@ import { createTodoElement } from "./scripts/createTodoElement.js";
 // What elements do we need?
 // What events do we need to listen for?
 // What should happen when those event occur?
+
+
+const inputText = document.querySelector("#todoTextInput");
+const addButton = document.querySelector("#addButton");
+const todoDisplay = document.querySelector("#todoDisplay");
+
+function updateDisplay() {
+    todoDisplay.innerHTML = "";
+    const newTodo = createTodoElement();
+    todoDisplay.appendChild(newTodo);
+}
+updateDisplay();
